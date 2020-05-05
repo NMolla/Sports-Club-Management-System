e731c88e2575ed09d80a048b29bc3e724b27b99d
NahomMolla <Nahom Molla>
2020-05-05 17:54:30 +0400

    Revert "x"
    This reverts commit cba7839b

--- Sport Management System/init.py
+++ Sport Management System/init.py
@@ -1,20 +1,20 @@
-#Import Flask Library
+# Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 
-#Initialize the app from Flask
+# Initialize the app from Flask
 app = Flask(__name__)
 
-#Configure MySQL
+# Configure MySQL
 conn = pymysql.connect(host='localhost',
-                       port = 3306,
+                       port=3306,
                        user='root',
                        password='',
                        db='SportsManagement',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
-#Define a route to hello function
+# Define a route to hello function
 @app.route('/')
 def hello():
     return render_template('index.html')
@@ -25,13 +25,9 @@ def login():
     return render_template('login.html')
 
 #Define route for register
-@app.route('/registerAsAthlete')
-def registerAsAthlete():
-    return render_template('registerAsAthlete.html')
-
-@app.route('/registerAsCoach')
-def registerAsCoach():
-    return render_template('registerAsCoach.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
@@ -43,7 +39,7 @@ def loginAuth():
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
-    query = 'SELECT * FROM users WHERE username = %s and password = %s and role = "athlete"'
+    query = 'SELECT * FROM user WHERE username = %s and password = %s'
     cursor.execute(query, (username, password))
     #stores the results in a variable
     data = cursor.fetchone()
@@ -54,56 +50,23 @@ def loginAuth():
         #creates a session for the the user
         #session is a built in
         session['username'] = username
-        return redirect(url_for('athlete'))
+        return redirect(url_for('home'))
     else:
-        cursor = conn.cursor()
-        # executes query
-        query = 'SELECT * FROM users WHERE username = %s and password = %s and role = "coach"'
-        cursor.execute(query, (username, password))
-        # stores the results in a variable
-        data = cursor.fetchone()
-        # use fetchall() if you are expecting more than 1 data row
-        cursor.close()
-        if (data):
-            # creates a session for the the user
-            # session is a built in
-            session['username'] = username
-            return redirect(url_for('coach'))
-        else:
-            cursor = conn.cursor()
-            # executes query
-            query = 'SELECT * FROM users WHERE username = %s and password = %s and role = "admin"'
-            cursor.execute(query, (username, password))
-            # stores the results in a variable
-            data = cursor.fetchone()
-            # use fetchall() if you are expecting more than 1 data row
-            cursor.close()
-            if (data):
-                # creates a session for the the user
-                # session is a built in
-                session['username'] = username
-                return redirect(url_for('admin'))
-            else:
-                #returns an error message to the html page
-                error = 'Invalid login or username'
-                return render_template('login.html', error=error)
+        #returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
 
 #Authenticates the register
-@app.route('/registerAuthAthlete', methods=['GET', 'POST'])
-def registerAuthAthlete():
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
     #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
-    firstName = request.form['firstName']
-    lastName = request.form['lastName']
-    phoneNumber = request.form['phoneNumber']
-    email = request.form['email']
-    role = 'athlete'
 
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
-    query = 'SELECT * FROM users WHERE username = %s'
+    query = 'SELECT * FROM user WHERE username = %s'
     cursor.execute(query, (username))
     #stores the results in a variable
     data = cursor.fetchone()
@@ -112,41 +75,10 @@ def registerAuthAthlete():
     if(data):
         #If the previous query returns data, then user exists
         error = "This user already exists"
-        return render_template('registerAsAthlete.html', error = error)
+        return render_template('register.html', error = error)
     else:
-        ins = 'INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password, firstName, lastName, phoneNumber, email, role))
-        conn.commit()
-        cursor.close()
-        return render_template('index.html')
-
-@app.route('/registerAuthCoach', methods=['GET', 'POST'])
-def registerAuthCoach():
-    #grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-    firstName = request.form['firstName']
-    lastName = request.form['lastName']
-    phoneNumber = request.form['phoneNumber']
-    email = request.form['email']
-    role = 'coach'
-
-    #cursor used to send queries
-    cursor = conn.cursor()
-    #executes query
-    query = 'SELECT * FROM users WHERE username = %s'
-    cursor.execute(query, (username))
-    #stores the results in a variable
-    data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
-    error = None
-    if(data):
-        #If the previous query returns data, then user exists
-        error = "This user already exists"
-        return render_template('registerAsCoach.html', error = error)
-    else:
-        ins = 'INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password, firstName, lastName, phoneNumber, email, role))
+        ins = 'INSERT INTO user VALUES(%s, %s)'
+        cursor.execute(ins, (username, password))
         conn.commit()
         cursor.close()
         return render_template('index.html')
@@ -162,10 +94,6 @@ def updateCoachSalary():
 @app.route('/displayFinancialReport')
 def displayFinancialReport():
     return render_template('displayFinancialReport.html')
-
-@app.route('/coach')
-def coach():
-    return render_template('coachHome.html')
 
 @app.route('/athlete')
 def athlete():
