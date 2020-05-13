773bebce5a9ffbeff7fa7400a17dcef6a67420de
NahomMolla <Nahom Molla>
2020-05-13 21:06:17 +0400

    table definitions update

--- table definitions.sql
+++ table definitions.sql
@@ -9,29 +9,16 @@ create table Users(
 	primary key(username)
 );
 
-
 create table Sports(
-	id varchar(255),
     name varchar(255),
     coach varchar(255),
-    primary key(id, name)
+    primary key(name)
 );
 
 create table Equipments(
 	id varchar(255),
     name varchar(255),
     primary key(id)
-);
-
-create table Teaches(
-	coachID varchar(255),
-    athleteID varchar(255),
-    sportID varchar(255),
-    time varchar(255),
-    primary key(coachID, sportID, time),
-    foreign key(coachID) references users(username),
-    foreign key(athleteID) references users(username),
-    foreign key(sportID) references sports(id)
 );
 
 create table CheckedEquipments(
@@ -57,10 +44,13 @@ create table MembershipFee(
 );
 
 create table Classes(
-  athlete varchar(255),
-  coach varchar(255),
-  day varchar(255),
-  time varchar(255),
-  sport varchar(255),
-  primary key(athlete,day,time)
+	athleteID varchar(255),
+	coachID varchar(255),
+    sportID varchar(255),
+	day varchar(255),
+	time varchar(255),
+	primary key(athleteID, day, time)
+    foreign key athleteID references users(username),
+    foreign key coachID references users(username),
+    foreign key sportID references Sports(name)
 );
